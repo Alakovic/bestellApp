27fce23dee7852aedcbf189ff81bfc8bb957b845
Zeljko Alakovic <zeljkoalakovic997@outlook.com>
2024-11-22 18:23:29 +0100

    header added

---
+++ scripts/db.js
@@ -0,0 +1,27 @@
+let myDishes = [
+    {
+        "name" : "Pizza Margherita",
+        "price" : 8.99,
+        "description" : "Klassische italienische Pizza mit frischen Tomaten, Mozzarella und Basilikum auf dünnem Teig."
+    },
+    {
+        "name" : "Pizza Salami",
+        "price" : 9.49 ,
+        "description" : " Herzhaft belegt mit würziger Salami, Mozzarella und hausgemachter Tomatensauce."
+    },
+    {
+        "name" : "Pizza Funghi",
+        "price" : 9.29 ,
+        "description" : "Vegetarische Pizza mit frischen Champignons, Mozzarella und einer leichten Knoblauchnote."
+    },
+    {
+        "name" : "Pizza Quattro Formaggi" ,
+        "price" : 10.49 ,
+        "description" : "Für Käseliebhaber – mit Mozzarella, Gorgonzola, Parmesan und Emmentaler auf knusprigem Teig."
+    },
+    {
+        "name" : "Pizza Prosciutto e Rucola" ,
+        "price" : 11.49,
+        "description" : "Mit feinem Parmaschinken, frischem Rucola, Mozzarella und einem Hauch von Olivenöl."
+    }
+];
